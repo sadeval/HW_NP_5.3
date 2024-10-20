1dcc409f814aeacbe9621d0975092df199c52f92
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-20 11:10:02 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace HamletViewerApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.TextBox textBoxHamlet;
+
+        /// <summary>
+        /// Освобождение ресурсов.
+        /// </summary>
+        /// <param name="disposing">true, если управляемые ресурсы должны быть удалены; иначе false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// Инициализация компонентов формы.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.textBoxHamlet = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 12);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(150, 58);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Загрузить текст Гамлета";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // textBoxHamlet
+            // 
+            this.textBoxHamlet.Location = new System.Drawing.Point(12, 76);
+            this.textBoxHamlet.Multiline = true;
+            this.textBoxHamlet.Name = "textBoxHamlet";
+            this.textBoxHamlet.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxHamlet.Size = new System.Drawing.Size(1044, 431);
+            this.textBoxHamlet.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 519);
+            this.Controls.Add(this.textBoxHamlet);
+            this.Controls.Add(this.loadButton);
+            this.Name = "Form1";
+            this.Text = "Hamlet Viewer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HamletViewerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void loadButton_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.gutenberg.org/files/1524/1524-0.txt"; 
+
+            try
+            {
+                string hamletText = await LoadHamletTextAsync(url);
+                textBoxHamlet.Text = hamletText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке текста: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task<string> LoadHamletTextAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string textContent = await response.Content.ReadAsStringAsync();
+                return textContent;
+            }
+        }
+    }
+}
